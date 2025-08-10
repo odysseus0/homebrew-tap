a74794872c01bb70bc948a395cc330bcd3218ccb
George Zhang <georgezhangtj97@gmail.com>
2025-08-09 17:39:26 -0700

    appearance-notify: add/update to v0.2.0

--- Formula/appearance-notify.rb
+++ Formula/appearance-notify.rb
@@ -3,22 +3,15 @@ class AppearanceNotify < Formula
   homepage "https://github.com/odysseus0/appearance-notify"
   version "0.2.0"
   license "MIT"
-  
+
+  url "https://github.com/odysseus0/appearance-notify/releases/download/v0.2.0/appearance-notify-apple-darwin.tar.gz"
+  sha256 "fe805fa107bfbe6739190b94287199162d035acf7f00f25597b398b8c1265058" # universal
+
   depends_on macos: :sonoma
-  
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/odysseus0/appearance-notify/releases/download/v0.2.0/appearance-notify-aarch64-apple-darwin.tar.gz"
-      sha256 "96503b5b86d2b84f1c92081e8112f9c60ec9d02c59b7a18d1b3094485f610afc"
-    else
-      url "https://github.com/odysseus0/appearance-notify/releases/download/v0.2.0/appearance-notify-x86_64-apple-darwin.tar.gz"
-      sha256 "a5bdc2f6f7d81be75d9869a6e72f703c1eabe17d437488248b7d7917bebd9d9e"
-    end
-  end
-  
+
   def install
     bin.install "appearance-notify"
-    
+
     (prefix/"io.github.odysseus0.appearance-notify.plist").write <<~EOS
       <?xml version="1.0" encoding="UTF-8"?>
       <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
@@ -39,12 +32,12 @@ class AppearanceNotify < Formula
       </plist>
     EOS
   end
-  
+
   service do
     run [opt_bin/"appearance-notify", "daemon"]
     keep_alive true
   end
-  
+
   test do
     assert_match version.to_s, shell_output("#{bin}/appearance-notify --version")
   end
