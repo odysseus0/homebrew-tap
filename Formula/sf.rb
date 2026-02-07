f65c9c27c12df541fbe67b2f5c8d3c6b3b018d55
George Zhang <georgezhangtj97@gmail.com>
2026-02-06 23:04:02 -0800

    sf: v0.1.1

--- Formula/sf.rb
+++ Formula/sf.rb
@@ -1,8 +1,8 @@
 class Sf < Formula
   desc "Index-powered file search for macOS (Spotlight-backed)"
   homepage "https://github.com/odysseus0/sf"
-  url "https://github.com/odysseus0/sf/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "2491b061ff1a81c41bec780e736ecf296d52cbd725783378a5a0c3bd438a3679"
+  url "https://github.com/odysseus0/sf/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "810ad411749d83d1cd091d4a5681f8a4c2a39c971944eeac26d56f8065cd4728"
   license "MIT"
 
   depends_on "rust" => :build
